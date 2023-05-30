4edcf0e56cbcd88026547c5a3c3a83fb0850d599
sophia <zchen9523@gmail.com>
2023-05-30 17:54:25 -0500

    some changes

--- frame.js
+++ frame.js
@@ -1,8 +1,8 @@
 var albumElement = document.getElementById("album");
-var names = ["winter", "snow", "cold", "ice cream", "frozen", "tree", "asshole"];
-var des = ['Winter is a cold season.', 'It generally starts in November.', "People use sweaters and blankets.", "Christmas, New Year, etc fall in winter.", "People enjoy carrots, peas, cauliflowers, etc in winter Season.", "Winter is the coldest season of the year."];
+var names = ["winter", "snow", "cold", "ice cream", "frozen", "tree", "asshole","lazy", "laying down", "hug"];
+var des = ['Winter is a cold season.', 'It generally starts in November.', "People use sweaters and blankets.", "Christmas, New Year, etc fall in winter.", "People enjoy carrots, peas, cauliflowers, etc in winter Season.", "Winter is the coldest season of the year.","the tree has a good crop of berries in winter","The greatest risk is in fields where a winter cereal cover crop has been used.","In Kansas, I see that some of the winter wheat has been harvested.","Varieties of winter wheat used for grain may also be used for forage"];
 
-for (var i = 0; i < 6; i++) {
+for (var i = 0; i < 10; i++) {
     var li = document.createElement("li");
     li.className = "photo";
 
@@ -31,4 +31,5 @@ for (var i = 0; i < 6; i++) {
         var currentDescription = event.target.parentElement.querySelector(".description");
         currentDescription.style.display = "none";
     });
+
 }
